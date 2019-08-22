e564a50fcabde205de254450c6ba22509b3d725f
Archeane <jennyxu8448@gmail.com>
2019-08-22 18:33:16 -0400

    added register

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Landing from '../pages/Landing';
 import Account from '../pages/Account';
+import Registration from '../pages/Registration';
 
 class ReactRouter extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class ReactRouter extends React.Component {
       <React.Fragment>
         <Route exact path="/" component={Landing} />
         <Route path="/account" component={Account} />
+        <Route path="/register" exact component={Registration} />
       </React.Fragment>
     );
   }
