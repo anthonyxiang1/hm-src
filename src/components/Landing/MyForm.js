2670c197b152b04749d5759937ec4853917cdba7
Archeane <jennyxu8448@gmail.com>
2019-08-22 18:13:43 -0400

    landing

---
+++ src/components/Landing/MyForm.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {Form, Button, FormControl} from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+
+import axios from 'axios';
+
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.state = {
+      email: "",
+      password: "",
+      errMsg: ""
+    };
+  }
+
+  render() {
+    const { email, password, errMsg } = this.state;
+    return (
+      <Form inline onSubmit={this.handleLoginSubmit.bind(this)}>
+        <small>-{errMsg}-</small>
+        <Form.Group>
+            <FormControl
+            name="email"
+            type="email"
+            placeholder="Email"
+            required={true}
+            value={email}
+            onChange={this.handleLoginChange}
+            />
+        </Form.Group>
+        <Form.Group>
+            <FormControl
+            name="password"
+            type="password"
+            placeholder="Password"
+            required={true}
+            value={password}
+            onChange={this.handleLoginChange}
+            />
+            <Button type="submit">Login</Button>
+        </Form.Group>
+        
+      </Form>
+    );
+  }
+
+  handleLoginChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  token(){
+    console.log(localStorage.getItem('auth_token'));
+  };
+
+  handleLoginSubmit(event){
+    event.preventDefault();
+    axios.post(`http://127.0.0.1:5000/auth/login`, 
+      {
+        "email":"test@test.com",
+        "password":"test"
+      }).then(res => {
+        console.log(res);
+        console.log(res.data);
+        console.log(res.data['auth_token']);
+        localStorage.setItem('auth_token', res.data['auth_token']);
+        
+      }).then(res => {this.props.history.push("/home");})
+      
+    
+    // var url = config.endpoint + 'login';
+    // var data = JSON.stringify({"email":"abc@gmail.com", "password": "qwer"});
+    // store.dispatch(fetchItems(url, data, "POST"));
+  };
+
+
+}
+//export default MyForm;
+export default withRouter(MyForm);
--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import App from "../components/App";
-import Posts from "../components/Posts";
-import Header from "../header";
+import Landing from '../pages/Landing';
 
 class ReactRouter extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Header />
-        <Route exact path="/" component={App} />
-        <Route  path="/posts" component={Posts} />
+        <Route exact path="/" component={Landing} />
       </React.Fragment>
     );
   }
